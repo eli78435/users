45f35f515fbea816b24b3646e2cf3a9e25bf0c7f
Eli Dagan <eli78435@gmail.com>
2023-05-02 16:11:51 +0300

    init project with material and firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'userseli',
+    appId: '1:1004394553119:web:328d74513bb54542c15a2a',
+    storageBucket: 'userseli.appspot.com',
+    apiKey: 'AIzaSyCQj6n-WsD-J4vU0fpsrNmb2pkDmv0cy1Q',
+    authDomain: 'userseli-b12c2.firebaseapp.com',
+    messagingSenderId: '1004394553119',
+  },
+    production: true
+};
