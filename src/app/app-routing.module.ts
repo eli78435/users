af3575f9c4c31e168704df62cfc74123a2a2e6d3
Eli Dagan <eli78435@gmail.com>
2023-05-03 20:11:00 +0300

    add navigation guard if the user is not signed in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,15 @@ import { ManageOrdersComponent } from './components/admin/manage-orders/manage-o
 import { ManageProductsComponent } from './components/admin/manage-products/manage-products.component';
 import { NoAccessComponent } from './components/no-access/no-access.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'shopping-card', component: ShoppingCartComponent },
-  { path: 'orders', component: OrdersComponent },
-  { path: 'manage-orders', component: ManageOrdersComponent },
-  { path: 'manage-products', component: ManageProductsComponent },
+  { path: 'shopping-card', component: ShoppingCartComponent, canActivate: [AuthGuard] },
+  { path: 'orders', component: OrdersComponent, canActivate: [AuthGuard] },
+  { path: 'manage-orders', component: ManageOrdersComponent, canActivate: [AuthGuard] },
+  { path: 'manage-products', component: ManageProductsComponent, canActivate: [AuthGuard] },
   { path: 'no-access', component: NoAccessComponent },
   { path: '*', component: NotFoundComponent }
 ];
